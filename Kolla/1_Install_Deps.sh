0528fb0e042cc51ed99895879a61046a809115f0
Rick Donato <rickj07@gmail.com>
2018-06-16 23:09:30 +0100

    Delete 1_Install_Deps.sh

--- Kolla/1_Install_Deps.sh
+++
@@ -1,7 +0,0 @@
-# perform deployment node
-yum install epel-release
-yum install python-pip
-pip install -U pip
-yum install python-devel libffi-devel gcc openssl-devel libselinux-python
-yum install ansible
-pip install -U ansible
