58a77183ce51bd3c1fe37fff57005d5bbc5db0d5
Rick Donato <rickj07@gmail.com>
2018-06-19 21:32:57 +0100

    Delete 3_Deploy.sh

--- Kolla/3_Deploy.sh
+++
@@ -1,8 +0,0 @@
-kolla-ansible -i ./multinode bootstrap-servers
-kolla-ansible -i ./multinode prechecks
-kolla-ansible -i ./multinode deploy
-
-kolla-ansible post-deploy
-. /etc/kolla/admin-openrc.sh
-
-pip install python-openstackclient python-glanceclient python-neutronclient
