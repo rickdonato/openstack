40cdd584c482609c1da6e5ab30b42dc577ce450c
Rick Donato <rickj07@gmail.com>
2018-06-19 21:32:45 +0100

    Delete 2_Install_KollaAnsible.sh

--- Kolla/2_Install_KollaAnsible.sh
+++
@@ -1,5 +0,0 @@
-pip install kolla-ansible
-cp -r /usr/share/kolla-ansible/etc_examples/kolla /etc/
-cp /usr/share/kolla-ansible/ansible/inventory/* .
-cp <file_in_repo> /usr/share/kolla-ansible/ansible/inventory/
-kolla-genpwd # /etc/kolla/passwords.yml
